da7170688015bb8798dd8eca2592fb685b14035a
evdlynch-creator <evdlynch@gmail.com>
2026-04-17 15:04:50 -0400

    [Blink] Fixed ReferenceError: useQueryClient is not defined • Added missing useQueryClient import in src/hooks/useTeam.ts • Verified all other files using useQueryClient have the correct import from @tanstack/react-query

--- src/hooks/useTeam.ts
+++ src/hooks/useTeam.ts
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { useQuery } from '@tanstack/react-query'
+import { useQuery, useQueryClient } from '@tanstack/react-query'
 import { blink } from '../blink/client'
 import { useAuth } from './useAuth'
 import type { Team, Season } from '../types'
